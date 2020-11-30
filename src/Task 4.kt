9e0c2bb7ad865c15c81cf5b3b4e7659d8a409acf
glebboos <boosgleb@gmail.com>
2020-11-30 19:33:00 +0300

    Добавлена 22 задачка

--- src/Task 4.kt
+++ src/Task 4.kt
@@ -1,6 +1,67 @@
 fun main(args: Array<String>) {
     var K1:Boeing747=Boeing747(1000)
     var K2:Airbus=Airbus(2000)
+    K1.display()
+    K2.display()
+}
+
+interface Passenger{
+    val _vmest_pass:Int
+}
+
+abstract class Aircraft constructor(nom_sud:Int, max_dal:Double,vm_bal:Double){
+    val _nom_sud:Int
+    init{
+        _nom_sud=nom_sud
+    }
+
+    val _max_dal: Double
+    init{
+        _max_dal=max_dal
+    }
+
+    val _vm_bal: Double
+    init{
+        _vm_bal=vm_bal
+    }
+
+    val ras_top: Double
+        get()=_vm_bal/_max_dal //расход литров на км
+
+    open internal fun display(){   // функция видна в текущем модуле
+
+        println("Номер судна: $_nom_sud  Максимальная дальность: $_max_dal Вместимость бака: $_vm_bal Расход литров на км: $ras_top")
+
+    }
+}
+
+class Boeing747(vmest_pass:Int):Aircraft(nom_sud=415,max_dal = 100000.0,vm_bal = 500.0),Passenger{
+    override val _vmest_pass:Int
+    init
+    {
+        _vmest_pass=vmest_pass
+    }
+    override fun display() {
+        println("Номер судна: $_nom_sud  Максимальная дальность: $_max_dal Вместимость бака: $_vm_bal Расход литров на км: $ras_top")
+    }
+}
+
+class Airbus(vmest_pass:Int):Aircraft(nom_sud=744,max_dal = 100000.0,vm_bal = 800.0),Passenger{
+    override val _vmest_pass:Int
+    init
+    {
+        _vmest_pass=vmest_pass
+    }
+}
+
+
+
+//Задача 21
+
+/*
+fun main(args: Array<String>) {
+    var K1:Boeing747=Boeing747(1000)
+    var K2:Airbus=Airbus(2000)
     println(K1.ras_top)
     println(K2.ras_top)
 }
@@ -44,6 +105,7 @@ class Airbus(vmest_pass:Int):Aircraft(nom_sud=744,max_dal = 100000.0,vm_bal = 80
         _vmest_pass=vmest_pass
     }
 }
+ */
 
 
 //Задача 20
